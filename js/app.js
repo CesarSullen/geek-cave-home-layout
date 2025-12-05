f3b4e583c6ceaddf1cd56128bf280fe03771e71f
saru <tools.customary833@passfwd.com>
2025-12-05 08:59:56 -0500

    added navbar

--- js/app.js
+++ js/app.js
@@ -1,3 +1,33 @@
+// Nav Bar Toggle
+const toggleBtn = document.getElementById("toggle-btn");
+const bottomPanel = document.getElementById("bottom-panel");
+const body = document.body;
+
+toggleBtn.addEventListener("click", () => {
+	toggleBtn.classList.toggle("toggled");
+
+	if (
+		bottomPanel.style.transform === "translateY(100%)" ||
+		bottomPanel.style.transform === ""
+	) {
+		bottomPanel.style.transform = "translateY(0%)";
+	} else {
+		bottomPanel.style.transform = "translateY(100%)";
+	}
+
+	body.style.overflowY = toggleBtn.classList.contains("toggled")
+		? "hidden"
+		: "auto";
+});
+
+document.querySelectorAll(".panel-link").forEach((link) => {
+	link.addEventListener("click", () => {
+		toggleBtn.classList.remove("toggled");
+		body.style.overflowY = "auto";
+		bottomPanel.style.transform = "translateY(100%)";
+	});
+});
+
 // Interception Observer
 const animatedElements = document.querySelectorAll(
 	".show-up, .show-down, .show-left, .show-right, .bounce-in, .rotate-left, .rotate-right"
